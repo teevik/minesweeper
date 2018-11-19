69c853f9b0a26ebb2f7137be83296761d12914fb
teevik <teemu.vikoren@gmail.com>
2018-11-19 23:05:13 +0100

    Don't ignore build folder

---
+++ build/precache-manifest.383bb1749a6f40030c342af0db9535dd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "df6ac6970f1e4bf744c9",
+    "url": "/minesweeper/static/js/main.df6ac697.chunk.js"
+  },
+  {
+    "revision": "1a55cb9560c7fa2be52f",
+    "url": "/minesweeper/static/js/1.1a55cb95.chunk.js"
+  },
+  {
+    "revision": "fc612313122b3cb541338a7ea87d708b",
+    "url": "/minesweeper/index.html"
+  }
+];
