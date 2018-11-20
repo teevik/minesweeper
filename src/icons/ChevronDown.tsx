feb0056d48224122a91bd48b6685836f4ca5818a
teevik <teemu.vikoren@gmail.com>
2018-11-20 21:01:11 +0100

    ChevronDown icon

---
+++ src/icons/ChevronDown.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import styled from "styled-components"
+
+export const ChevronDown: React.FC = props => {
+  return (
+    <Wrapper xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.829 29.828">
+      <Path d="M6,9l8.725,8.725L23.451,9" />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.svg`
+  opacity: 0.83;
+  width: 20px;
+`
+
+const Path = styled.path`
+  fill: none;
+  stroke: #5258d4;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-width: 2px;
+`
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
+export * from "./ChevronDown"
 export * from "./CloseIcon"
 export * from "./FlagIcon"
 export * from "./RefreshIcon"
