79a4cda6adeddc77a1bd174953fac4ae8ddc1441
teevik <teemu.vikoren@gmail.com>
2018-11-20 21:20:40 +0100

    Updates

---
+++ precache-manifest.692472cc54b21b5f49d8445e6756e100.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "c0f30680b06f7533c552",
+    "url": "/minesweeper/static/js/main.c0f30680.chunk.js"
+  },
+  {
+    "revision": "eea2089e9cfc813a1233",
+    "url": "/minesweeper/static/js/1.eea2089e.chunk.js"
+  },
+  {
+    "revision": "50e23a70d5c81e60b106b874773dfd8b",
+    "url": "/minesweeper/index.html"
+  }
+];
