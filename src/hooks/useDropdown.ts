ce212d35bbb138c422cfe54837599a92886d73a8
teevik <teemu.vikoren@gmail.com>
2018-11-21 19:22:17 +0100

    use `useCallback`

--- src/hooks/useDropdown.ts
+++ src/hooks/useDropdown.ts
@@ -1,12 +1,12 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { DropdownEntry } from "../types"
 
 export const useDropdown = (entries: DropdownEntry[]) => {
   const [selectedEntry, setSelectedEntry] = useState(entries[0])
 
-  const onEntryChange = (entry: DropdownEntry) => {
+  const onEntryChange = useCallback((entry: DropdownEntry) => {
     setSelectedEntry(entry)
-  }
+  }, [])
 
   return { entries, selectedEntry, onEntryChange }
 }
