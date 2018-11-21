50cb1bb26e1870febacb2ef08d043f60c27c0ab0
teevik <teemu.vikoren@gmail.com>
2018-11-21 19:28:29 +0100

    Add inputs for custom game settings

--- src/containers/StartScreen.tsx
+++ src/containers/StartScreen.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import styled from "styled-components"
 import { Button, Dropdown, Label } from "../components"
 import { boardInfo } from "../constants"
-import { useDropdown } from "../hooks"
+import { useDropdown, useNumberInput } from "../hooks"
 import { gameStore } from "../stores"
 
 export const StartScreen: React.FC<
@@ -11,20 +11,46 @@ export const StartScreen: React.FC<
   const gameModeDropdown = useDropdown([
     { value: "beginner", content: "Beginner - 9x9" },
     { value: "advanced", content: "Advanced - 16x16" },
-    { value: "expert", content: "Expert - 30x16" }
+    { value: "expert", content: "Expert - 30x16" },
+    { value: "custom", content: "Custom" }
   ])
+  const [widthInput, width] = useNumberInput(1)
+  const [heightInput, height] = useNumberInput(1)
+  const [bombAmountInput, bombAmount] = useNumberInput(1)
 
   const { selectedEntry } = gameModeDropdown
+  const isCustom = selectedEntry.value === "custom"
 
   const handleStartGame = () => {
-    gameStore.setupGame(boardInfo[selectedEntry.value])
+    if (isCustom) {
+      gameStore.setupGame({
+        width,
+        height,
+        bombAmount
+      })
+    } else {
+      gameStore.setupGame(boardInfo[selectedEntry.value])
+    }
     gameStore.onStartGame()
   }
 
+  const customSetup = isCustom && (
+    <>
+      <Label>Width</Label>
+      <input {...widthInput} />
+      <Label>Height</Label>
+      <input {...heightInput} />
+      <Label>Amount of mines</Label>
+      <input {...bombAmountInput} />
+    </>
+  )
+
   return (
-    <Wrapper>
+    <Wrapper {...props}>
       <Label>Game mode</Label>
       <Dropdown {...gameModeDropdown} />
+
+      {customSetup}
 
       <Hr />
 
