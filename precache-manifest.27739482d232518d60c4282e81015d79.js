f7981225d352853aeda6478d338068bb6d0835e8
teevik <teemu.vikoren@gmail.com>
2018-11-21 21:53:50 +0100

    Updates

---
+++ precache-manifest.27739482d232518d60c4282e81015d79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "3d2f8243a1738c79c372",
+    "url": "/minesweeper/static/js/main.3d2f8243.chunk.js"
+  },
+  {
+    "revision": "e98fb22ecd0c7387db64",
+    "url": "/minesweeper/static/js/1.e98fb22e.chunk.js"
+  },
+  {
+    "revision": "879c7c8badbc1d51e945de33c93066d5",
+    "url": "/minesweeper/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/minesweeper/precache-manifest.d3e63deabe7b3ee7a18d1a85a0aa4834.js"
+  "/minesweeper/precache-manifest.27739482d232518d60c4282e81015d79.js"
 );
 
 workbox.clientsClaim();
